86764b14aa4baeca387010fd0ce082c89309ba7e
MattIRio <matlirioiii@gmail.com>
2024-11-29 15:10:24 +0200

    figgured out css

--- src/main/java/newproject/newproject/page.java
+++ src/main/java/newproject/newproject/page.java
@@ -1,14 +1,13 @@
 package newproject.newproject;
 
 // import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "https://matthewlocalhost")
-@RestController
-@RequestMapping("/api")
+@Controller
 public class page {
 
     @GetMapping("/mainpage")
