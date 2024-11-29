7db3061018164551d6673b25e84da119e2ef6939
VolKuchyn <i.m.devil0906@gmail.com>
2024-11-29 15:00:27 +0200

    add

--- src/main/java/newproject/newproject/page.java
+++ src/main/java/newproject/newproject/page.java
@@ -1,6 +1,6 @@
 package newproject.newproject;
 
-import org.springframework.stereotype.Controller;
+// import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
