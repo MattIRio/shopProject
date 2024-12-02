6af87eb99c978ccd7d5977bfdbae48052cf35b4b
MattIRio <matlirioiii@gmail.com>
2024-12-02 17:40:09 +0200

    added getproducts

--- src/main/java/newproject/newproject/ObjectsArray.java
+++ src/main/java/newproject/newproject/ObjectsArray.java
@@ -1,38 +1,34 @@
 package newproject.newproject;
 
-import newproject.newproject.model.userModel;
+import newproject.newproject.model.ProductModel;
+import newproject.newproject.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
 @RestController
     public class ObjectsArray {
+    @Autowired
+    ProductRepository productRepository;
 
-        @GetMapping("/getobjects")
-        public ResponseEntity<List<userModel>> teammembers() {
-            userModel user = new userModel("Van", 300, "Master", false);
-            userModel user1 = new userModel("Bill", 35, "Master", false);
-            userModel user2 = new userModel("Mark", 36, "Master", false);
-            userModel user3 = new userModel("Bogdan", 40, "Master", true);
-
-            List<userModel> usersArray = new ArrayList<>();
-
-            usersArray.add(user1);
-            usersArray.add(user);
-            usersArray.add(user2);
-            usersArray.add(user3);
-
-            
+        @GetMapping("/getallproducts")
+        public ResponseEntity<List<ProductModel>> allProducts() {
 
 
+            List<ProductModel> productList = productRepository.findAll();
 
+            return ResponseEntity.ok(productList.subList(0, 20));
+        }
 
-            return ResponseEntity.ok(usersArray);
+        @PutMapping("/saveproduct")
+    public ResponseEntity<String> saveProduct(){
+            productRepository.save(new ProductModel("awdaw12", "Alisha Solid Women's Cycling Shorts", 999, 379, "url_to_image", "Описание продукта", "awdaw"));
+
+            return ResponseEntity.ok("saved");
         }
     }
