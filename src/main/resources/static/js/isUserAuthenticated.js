9cb821ef42fe5c921ecc4a7f65f66cc7b53b78dc
VolKuchyn <i.m.devil0906@gmail.com>
2025-01-05 21:24:31 +0200

    bahato chogo dobaviv, ne robe add item (transaction)

---
+++ src/main/resources/static/js/isUserAuthenticated.js
@@ -0,0 +1,34 @@
+async function uploadUserPhotoAndName() {
+    try {
+        const isUserAuthenticatedResponse = await fetch("/isuserauthenticated");
+        if (!isUserAuthenticatedResponse.ok) {
+            throw new Error(`Failed to check user authentication. Status: ${isUserAuthenticatedResponse.status}`);
+        }
+
+        const isUserAuthenticated = await isUserAuthenticatedResponse.json(); // Очікуємо true або false
+        if (!isUserAuthenticated) {
+            console.warn("User is not authenticated. Skipping user data upload.");
+            return;
+        }
+
+        const userDataFetch = await fetch("/getcurrentuserdata");
+        if (!userDataFetch.ok) {
+            throw new Error(`Failed to fetch user data. Status: ${userDataFetch.status}`);
+        }
+
+        const userData = await userDataFetch.json();
+        if (userData.profilePicture) {
+            let profilePicture = userData.profilePicture.split("/uploads")[1]; // Отримуємо все після '/uploads'
+            profilePicture = "/uploads" + profilePicture; // Додаємо '/uploads' назад
+            document.getElementById('default-user-icon').src = profilePicture;
+        }
+
+        if (userData.userName) {
+            document.getElementById('user-name-header').innerHTML = userData.userName;
+        }
+    } catch (error) {
+        console.error("Error during user data upload:", error);
+    }
+}
+
+uploadUserPhotoAndName();
